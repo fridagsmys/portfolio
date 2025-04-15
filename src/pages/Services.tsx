113ec625584a64efb570f54f1b49192653752e89
Frida <frida.alsterg@gmail.com>
2025-04-15 15:19:17 +0200

    commented out code

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import avatar from "../assets/avatar.jpeg";
-import Skills from "../components/Skills";
+// import Skills from "../components/Skills";
 
 const Services: React.FC = () => {
   return (
