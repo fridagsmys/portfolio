dd1bf7108d24c21646300e05a8e09f4beee7e21a
Frida <frida.alsterg@gmail.com>
2025-04-16 09:40:29 +0200

    url paths change

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,27 @@ import "./index.css";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Blog from "./pages/Blog";
-import Post from "./pages/Post";
 import About from "./pages/About";
-import GamePage from "./pages/GamePage";
 import Connect from "./pages/Connect";
 import Projects from "./pages/Projects";
+// import Blog from "./pages/Blog";
+// import Post from "./pages/Post";
+// import GamePage from "./pages/GamePage";
 
 const App = () => {
   return (
-    <Router basename="/portfolio">
+    <Router>
       <nav>
         <Navbar />
       </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/connect" element={<Connect />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:id" element={<Post />} />
-        <Route path="/game" element={<GamePage />} />
+        <Route path="/portfolio/" element={<Home />} />
+        <Route path="/portfolio/about" element={<About />} />
+        <Route path="/portfolio/projects" element={<Projects />} />
+        <Route path="/portfolio/connect" element={<Connect />} />
+        {/* <Route path="/portfolio/blog" element={<Blog />} />
+        <Route path="/portfolio/blog/:id" element={<Post />} />
+        <Route path="/portfolio/game" element={<GamePage />} /> */}
       </Routes>
         <Footer />
     </Router>
